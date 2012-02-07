bac77dd0282fa83a7712c0487f131e1272883fe4
Chris Ober <ober@princeton.edu>
2012-02-07 10:11:28 -0500

    .

--- quotas.py
+++ quotas.py
@@ -10,19 +10,22 @@ class User:
     return self.quota
     
     
-
 def main(addr, passwd, binddn, base):
   print("Querying LDAP Server %s..." %addr)
   cmdline = ['ldapsearch',  '-x', '-LLL', '-D %s' % binddn, '-b %s' % base, '-H %s' %addr, '-w %s' % passwd]
   try: 
-    rawldif = Popen(cmdline, shell=True, stdout=PIPE, stderr=PIPE, executable='/bin/bash')
-    print(rawldif.stdout.read())
+    rawldif = Popen(cmdline, shell=True, stdout=PIPE, stderr=STDOUT, executable='/bin/bash')
+    out = rawldif.stdout.read()
+    print(out)
+  except TypeError as err:
+    print("%s\nCould not query LDAP Server `%s`" % (err, addr))
   except:
-    print("Could not query LDAP Server %s" %addr)
+    print("Unhandled exception %s\nCould not query LDAP Server `%s`" % (sys.exc_info(), addr))
 
 if __name__ == '__main__':
-  from subprocess import (PIPE, Popen, check_output)
-    
+  from subprocess import (PIPE, STDOUT, Popen, check_output)
+  import sys
+  
   readcfg = open('serverinfo.conf', 'r')
   server  = readcfg.readline().strip('\n')
   passwd  = readcfg.readline().strip('\n')
